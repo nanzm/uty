7364533791e66df678971d66e9f444fbcb50a0ad
huangchaoqun <huangchaoqun@xiaomai5.com>
2020-06-05 10:29:32 +0800

    feat: 添加函数

--- test/index.test.js
+++ test/index.test.js
@@ -1,12 +1,30 @@
-import { hi, getQuery } from '../src/index'
+import uty, {getQuery} from '../src/index'
 
-test('isObject', () => {
-  expect(hi()).toEqual('hello uty')
+test('uty', () => {
+	const {
+		isArray,
+		isBoolean,
+		isDate,
+		isError,
+		isFunction,
+		isNull,
+		isObj,
+		isRegExp,
+		isPromise,
+		isUndefined
+	} = uty.type
+
+	expect(isArray([])).toEqual(true)
+	expect(isArray({})).toEqual(false)
+
+	expect(isBoolean(true)).toEqual(true)
+	expect(isBoolean(false)).toEqual(true)
+
+	expect(isDate(new Date())).toEqual(true)
+
+	expect(isError(new Error(""))).toEqual(true)
+
 })
 
 test('t1', () => {
-  console.log(getQuery(''))
-})
-
-test('t2', () => {
 })
